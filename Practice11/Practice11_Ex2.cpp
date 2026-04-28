cfcf739bd3bfb650dfd283f0b629a91b3b1e7880
Anastassija <annest@taltech.ee>
2026-04-29 01:56:20 +0300

    Practices 9, 10, 11 all done

--- Practice11/Practice11_Ex2.cpp
+++ Practice11/Practice11_Ex2.cpp
@@ -8,6 +8,20 @@ public:
     }
 };
 
+class NegativeValueException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Negative value is not allowed.";
+    }
+};
+
+class TooSmallException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Value is too small. Must be at least 10.";
+    }
+};
+
 int getValidInteger() {
     int value;
     int attempts = 0;
@@ -29,12 +43,25 @@ int getValidInteger() {
     }
 }
 
+void checkNumber(int x) {
+    if (x < 0) {
+        throw NegativeValueException();
+    } else if (x < 10) {
+        throw TooSmallException();
+    }
+}
+
 int main() {
     try {
         int result = getValidInteger();
+        checkNumber(result);
         std::cout << "Input was successful. Integer entered: " << result;
     } catch (const InvalidInputException& e) {
         std::cout << "Input error: " << e.what() << std::endl;
+    } catch (const NegativeValueException& e) {
+        std::cout << "Logic error: " << e.what() << std::endl;
+    } catch (const TooSmallException& e) {
+        std::cout << "Rule error: " << e.what() << std::endl;
     }
     return 0;
 }
